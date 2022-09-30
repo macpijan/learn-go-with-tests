f3c66a72f46682bbee3c4b729603fb89e838234d
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2022-09-30 20:59:39 +0200

    05_hellp_world_language: failing test
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ 05_hello_world_language/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name + "!"
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
