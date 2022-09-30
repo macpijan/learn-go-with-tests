6c6499747b23838683b43f3c5b6620459fb5754d
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2022-09-30 21:03:21 +0200

    05_hellp_world_language: refactor
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- 05_hello_world_language/hello.go
+++ 05_hello_world_language/hello.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
